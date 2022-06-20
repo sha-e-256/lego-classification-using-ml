b66fdb3b7f2b0f6d53da487b76ef6515aae4e447
sha-e-256 <105937174+sha-e-256@users.noreply.github.com>
2022-06-20 14:40:13 -0400

    replace file with correct dataset.py

--- dataset.py
+++ dataset.py
@@ -1,89 +1,90 @@
 import argparse
-import cv2
+import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
 import os
-import sys
 import random
 from tqdm import tqdm
 
 __author__ = "Shahed E."
-__date__ = "5/27/2022"
+__date__ = "6/11/2022"
 __status__ = "Development"
 
-# Note: Give variables short lives; define variables close to where you plan on using them
-# Note 2: Keep all class variables private
+class TrainingDataset:
+    # Class variables (belong to the class)
 
+    def __init__(self, dir_training_images):
+        # Instance variables (belong to each object)
+        self.dir_training_images = dir_training_images
+        self.enum_class_names = self.get_class_names(self.dir_training_images)
 
-class Dataset:
-    # Private class variables
-    images_dir = None
-    masks_dir = None
-    list_class_names = None
-    shuffle = False
-    list_training_dataset = []
-
-    def __init__(self, images_dir, masks_dir, shuffle):
-        # Class Variables
-        self.images_dir = images_dir
-        self.masks_dir = masks_dir
-        self.shuffle = shuffle
-        self.get_class_names()
-        self.generate_training_dataset()
-
-    def __getitem__(self, image_dir, mask_dir, index):
-        image = cv2.imread(image_dir)
-        image_greyscale = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-        np_array_image = np.array(image_greyscale, dtype='float32')  # Convert image into numpy array
-        mask = cv2.imread(mask_dir)
-        np_array_mask = np.array(mask)  # Convert mask into numpy array
-        np_array_image, __np_array_mask = self.normalize(np_array_image, np_array_mask, index)
-        return np_array_image, __np_array_mask
-
-    def get_np_array_images(self):
-        return self.list_training_dataset[0]
-
-    def get_np_array_masks(self):
-        return self.list_training_dataset[1]
-
-    def normalize(self, np_array_image, np_array_mask, index):
-        np_array_image = np_array_image
-        np_array_mask[np_array_mask == 255.000] = index + 1  # Consider 0 as the background class
-        # --> Creates an input segmentation map such that every pixel
-        # corresponding to a white pixel is given a class number
-        # Therefore, background class remains == 0
-        return np_array_image, np_array_mask
-
-    def get_class_names(self):
+    def get_class_names(self, dir_training_images):
         try:
-            self.list_class_names = os.listdir(self.images_dir)
+            list_element_id = os.listdir(self.dir_training_images)
+            enum_element_id  = enumerate(list_element_id)
         except FileNotFoundError as error:
             print(error)
+        return enum_element_id
 
-    def generate_training_dataset(self):
-        try:
-            for class_name in self.list_class_names:
-                class_name_path = os.path.join(self.images_dir, class_name)  # Go through folder of every piece
-                index = self.list_class_names.index(class_name)  # 0, 1, 2, ..., 9
-                for image_name in tqdm(os.listdir(class_name_path)):
-                    try:
-                        image_dir = os.path.join(class_name_path, image_name)
-                        mask_dir = os.path.join(self.masks_dir, f"{class_name}\mask-{image_name}")
-                        self.list_training_dataset.append(self.__getitem__(image_dir, mask_dir, index))
-                    except Exception as error:  # Ignore corrupted images
-                        print(error)
-                        pass
-        except TypeError as error:
-            print(error)
+    def get_np_array_img(self, dir):
+        img = cv.imread(dir, 0)  # 0: image is converted to greyscale upon import
+        np_array_img = np.array(img, dtype='float32')
+        return np_array_img
 
-        if self.shuffle:
-            random.shuffle(self.list_training_dataset)
-        return
+    def smart_crop(self, img):
+        min_x, min_y, max_x, max_y, c_x, c_y = self.get_bounding_box(img)
+        bounding_img = img[min_y:max_y, min_x:max_x]
+        right = 70 - (max_x - c_x)
+        top = 70 - (c_y - min_y)
+        left = 70 - (c_x - min_x)
+        bottom = 70 - (max_y - c_y)
+        square_img = cv.copyMakeBorder(bounding_img, top, bottom, left, right, cv.BORDER_REPLICATE)
+        return square_img
+
+    def get_bounding_box(self, img):
+        img_g = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+        threshold, array_img = cv.threshold(img_g, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)
+        array_contours = cv.findContours(array_img, 1, 2)[0]
+        contour = array_contours[0]
+        x_min, y_min, width, height = cv.boundingRect(contour)  # Image only contains one contour
+        offset = 2
+        x_min -= offset
+        y_min -= offset
+        x_max = x_min + width + 2 * offset
+        y_max = y_min + height + 2 * offset
+        m = cv.moments(contour)
+        c_x = int(m['m10'] / m['m00'])
+        c_y = int(m['m01'] / m['m00'])
+        return x_min, y_min, x_max, y_max, c_x, c_y
+
+    def traverse_dir_training_images(self, op, dir_dst):
+        for label, element_id in self.enum_class_names:
+            dir_element_id = rf'{self.dir_training_images}\{element_id}'
+            list_img = os.listdir(dir_element_id)
+            for img_name in list_img:
+                dir_img = rf'{dir_element_id}\{img_name}'
+                img = cv.imread(dir_img)
+
+                if op == 0:
+                    # Given a directory of images, crop image to have an aspect ratio of 1:1
+                    # and remove as much bg as possible
+                    square_img = self.smart_crop(img)
+                    cv.imwrite(dir_dst, square_img)
+
+                if op == 1:
+                    # Create training dataset
+                    np_array_img = self.get_np_array_image(dir_img)
+                    self.features.append(np_array_img)
+                    #self.labels.append(label)
+
 
 def main():
-    images_dir = r"D:\lego-classification-using-ml\training-images"  # Convert to raw string to ignore /t escape key
-    masks_dir = r'D:\lego-classification-using-ml\training-masks'
-    training_dataset = Dataset(images_dir, masks_dir, shuffle=True)
-    print(training_dataset.get_np_array_images())
+
+    images_dir = r'E:\lego-classification-using-ml\training-images'  # dir_src
+    squares_dir = r'E:\lego-classification-using-ml\square-training-images'  #dir_dst
+    training_dataset = TrainingDataset(images_dir)
+    training_dataset.traverse_dir_training_images(0, squares_dir)
+
+
+
 if __name__ == '__main__':
     main()
