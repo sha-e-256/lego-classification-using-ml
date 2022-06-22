875f327d35141808b7fcdd75320a1f265ecee048
sha-e-256 <105937174+sha-e-256@users.noreply.github.com>
2022-06-22 13:46:57 -0400

    save features and labels to .npy files

--- dataset.py
+++ dataset.py
@@ -1,6 +1,8 @@
-import argparse
 import cv2 as cv
+from IPython.display import display, Image
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import os
 import smart_crop as sc
 import random
@@ -17,6 +19,9 @@ class Dataset:
         # Instance variables (belong to each object)
         self.dir_training_images = dir_training_images
         self.enum_class_names = self.get_class_names(self.dir_training_images)
+        self.training_dataset = []
+        self.features = []
+        self.labels = []
 
     def get_class_names(self, dir_training_images):
         try:
@@ -25,10 +30,6 @@ class Dataset:
         except FileNotFoundError as error:
             print(error)
         return enum_element_id
-
-    def get_np_array_img(self, img):
-        np_array_img = np.array(img, dtype='float32')
-        return np_array_img
 
     def traverse_dir_training_images(self, dst_dir, op):
         flag = True
@@ -39,36 +40,45 @@ class Dataset:
                 dir_img = rf'{dir_element_id}\{img_name}'
                 img = cv.imread(dir_img)
 
+                # Given a directory of images, crop image to have an aspect ratio of 1:1
+                # and remove as much bg as possible
                 if op == 0:
-                    # Given a directory of images, crop image to have an aspect ratio of 1:1
-                    # and remove as much bg as possible
-                   if flag:
+
+                   if flag:  # If sub-folder has already been generated, do not re-generate
                         try:
                             os.mkdir(rf'{dst_dir}\{element_id}')
                         except OSError as error:
                             pass
                         square_img = sc.smart_crop(img)
                         dst_dir_img = rf'{dst_dir}\{element_id}\{img_name}'
-                        # cv.imshow('square', square_img)
-                        # cv.waitKey(0)
-                        # cv.destroyAllWindows()
                         cv.imwrite(dst_dir_img, square_img)
 
+                # Create training dataset
                 if op == 1:
-                    # Create training dataset
-                    np_array_img = self.get_np_array_img(img)
-                    self.features.append(np_array_img)
-                    #self.labels.append(label)
+                    self.training_dataset.append([img, label])
+                    # img is a NumPy array
+                    # More efficient to have list of NumPy arrays than a 2D NumPy array
+            if op == 1:
+                random.shuffle(self.training_dataset) # Shuffling allows the model to be trained more effectively
+                self.features = self.training_dataset[0]
+                self.labels = self.training_dataset[1]
+                np.save('features.npy', self.features)
+                np.save('labels.npy', self.labels)
 
+    def display_training_dataset(self):
+        pass
+        # TO DO: Display features & labels arrays in a dataframe
+        # dataframe = pd.DataFrame(self.labels)
+        # pd.options.display.max_columns = 20
+        # display(dataframe)
 
 def main():
 
     images_dir = r'D:\lego-classification-using-ml\training-images'  # src_dir
     squares_dir = r'D:\lego-classification-using-ml\square-training-images'  # dst_dir
     training_dataset = Dataset(images_dir)
-    training_dataset.traverse_dir_training_images(squares_dir, 0)
-
-
+    training_dataset.traverse_dir_training_images(squares_dir, 1)
+    training_dataset.display_training_dataset()
 
 if __name__ == '__main__':
     main()
