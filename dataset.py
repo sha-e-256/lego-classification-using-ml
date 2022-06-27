3e2b572fd7627f0a0ea793693718ebaa3f34bae9
sha-e-256 <105937174+sha-e-256@users.noreply.github.com>
2022-06-26 20:29:17 -0400

    fix #1

--- dataset.py
+++ dataset.py
@@ -9,8 +9,6 @@ import random
 from tqdm import tqdm
 
 __author__ = "Shahed E."
-__date__ = "6/11/2022"
-__status__ = "Development"
 
 class Dataset:
     # Class variables (belong to the class)
@@ -31,7 +29,18 @@ class Dataset:
             print(error)
         return enum_element_id
 
-    def generate_dataset(self, dst_dir, op):
+    def get_max_border(self):
+        max_border = 0
+        for label, element_id in self.enum_class_names:
+            dir_element_id = rf'{self.dir_training_images}\{element_id}'
+            list_img = os.listdir(dir_element_id)
+            for img_name in tqdm(list_img):
+                dir_img = rf'{dir_element_id}\{img_name}'
+                img = cv.imread(dir_img)
+                max_border = sc.find_max_border(max_border, min_x, min_y, max_x, max_y, c_x, c_y)
+        return max_border
+
+    def generate_dataset(self, dst_dir):
         flag = True
         for label, element_id in self.enum_class_names:
             dir_element_id = rf'{self.dir_training_images}\{element_id}'
@@ -39,35 +48,28 @@ class Dataset:
             for img_name in tqdm(list_img):
                 dir_img = rf'{dir_element_id}\{img_name}'
                 img = cv.imread(dir_img)
-
                 # Given a directory of images, crop image to have an aspect ratio of 1:1
                 # and remove as much bg as possible
-                if op == 0:
-                   if flag:  # If sub-folder has already been generated, do not re-generate
-                        try:
-                            os.mkdir(rf'{dst_dir}\{element_id}')
-                        except OSError as error:
-                            pass
-                        contours_array = sc.get_contours(img)
-                        square_img = sc.smart_crop(img, contours_array, 0)  # 0 for only one object in image
-                        dst_dir_img = rf'{dst_dir}\{element_id}\{img_name}'
-                        cv.imwrite(dst_dir_img, square_img)
+                if flag:  # If sub-folder has already been generated, do not re-generate
+                    try:
+                        os.mkdir(rf'{dst_dir}\{element_id}')
+                    except OSError as error:
+                        pass
+                    square_img = sc.smart_crop(75, img)  # 0 for only one object in image
+                    dst_dir_img = rf'{dst_dir}\{element_id}\{img_name}'
+                    cv.imwrite(dst_dir_img, square_img)
+                    self.training_dataset.append([img, label])  # More efficient to have list of NumPy arrays than a 2D NumPy array
 
-                # Create training dataset
-                if op == 1:
-                    self.training_dataset.append([img, label])
-                    # img is a NumPy array
-                    # More efficient to have list of NumPy arrays than a 2D NumPy array
-            if op == 1:
-                random.shuffle(self.training_dataset) # Shuffling allows the model to be trained more effectively
-                self.features = self.training_dataset[0]
-                self.labels = self.training_dataset[1]
-                np.save('features.npy', self.features)
-                np.save('labels.npy', self.labels)
+        random.shuffle(self.training_dataset) # Shuffling allows the model to be trained more effectively
+        self.features = self.training_dataset[0]
+        self.labels = self.training_dataset[1]
+        np.save('features.npy', self.features)
+        np.save('labels.npy', self.labels)
+
 
     def display_training_dataset(self):
         pass
-        # TO DO: Display features & labels arrays in a dataframe
+        # !!To do: Display features & labels arrays in a dataframe
         # dataframe = pd.DataFrame(self.labels)
         # pd.options.display.max_columns = 20
         # display(dataframe)
@@ -77,8 +79,8 @@ def main():
     images_dir = r'D:\lego-classification-using-ml\training-images'  # src_dir
     squares_dir = r'D:\lego-classification-using-ml\square-training-images'  # dst_dir
     training_dataset = Dataset(images_dir)
-    training_dataset.generate_dataset(squares_dir, 0)
-    training_dataset.display_training_dataset()
+    training_dataset.generate_dataset(squares_dir)
+    #training_dataset.display_training_dataset()
 
 if __name__ == '__main__':
     main()
