2a2ac4939f457705be8330c555cf496d9dfbfb3c
sha-e-256 <105937174+sha-e-256@users.noreply.github.com>
2022-07-12 12:50:51 -0400

    crop based off of rotated bounding box

--- smart_crop.py
+++ smart_crop.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import numpy as np
 
 __author__ = "Shahed E."
 
@@ -44,24 +45,43 @@ def find_max_border(max_border, min_x, min_y, max_x, max_y, c_x, c_y):
 
 # Create a square, cropped image based off of the bounding box enclosing the contour
 def smart_crop(img, max_border, dst_dir):
+    max_border = int(max_border)
     contours_array = get_contours(img)
-    for i in range(len(contours_array)):
+    for contour in contours_array:
         img_copy = img.copy() # Do not overwrite original image; crop the copies of the original image
-        min_x, min_y, max_x, max_y, c_x, c_y = get_bounding_box(contours_array[i])
-        cropped_img = img_copy[min_y:max_y, min_x:max_x]  # Return image within bounding box coordinates
-        right = max_border - (max_x - c_x)
-        top = max_border - (c_y - min_y)
-        left = max_border - (c_x - min_x)
-        bottom = max_border - (max_y - c_y)
+
+        b_rect = cv.minAreaRect(contour)  # The center coordinates, width, height, and angle of rotation of b box
+        rotation_matrix = cv.getRotationMatrix2D(center=b_rect[0], angle=b_rect[2], scale=1)
+        rotated_img = cv.warpAffine(src=img_copy, M=rotation_matrix, dsize=(1280, 720))
+        # Tuples are immutable
+        (c_x, c_y) = b_rect[0]  # Used for rotated and un-rotated images
+        c_x = int(c_x)
+        c_y = int(c_y)
+        (width, height) = b_rect[1]
+
+        rotated_b_rect = (b_rect[0], b_rect[1], 0)  # Bounding box of piece after rotating image
+        rotated_b_box = np.int0(cv.boxPoints(rotated_b_rect))  # The coordinates of the four corners of the bounding box
+        # print(b_box)
+        rotated_min_x = int(rotated_b_box[0, 0] - 2)
+        rotated_max_x = int(rotated_b_box[2, 0] + 4)
+        rotated_min_y = int(rotated_b_box[1, 1] - 2)
+        rotated_max_y = int(rotated_b_box[3, 1] + 4)
+
+        cropped_img = rotated_img[rotated_min_y:rotated_max_y, rotated_min_x:rotated_max_x]  # Return image within bounding box coordinates
+
+        right = max_border - (rotated_max_x - c_x)
+        top = max_border - (c_y - rotated_min_y)
+        left = max_border - (c_x - rotated_min_x)
+        bottom = max_border - (rotated_max_y - c_y)
         white = [255, 255, 255]
         square_img = cv.copyMakeBorder(cropped_img, top, bottom, left, right, cv.BORDER_CONSTANT, None, white)  # Add a white border
         if (len(contours_array) == 1):
             dst_dir_img = rf'{dst_dir}'
         else:
-            down_width = 150
-            down_height = 150
+            down_width = 256
+            down_height = 256
             down_points = (down_width, down_height)
-            dst_dir_img = rf'{dst_dir}\{i}.png'
+            dst_dir_img = rf'{dst_dir}\{contours_array.index(contour)}.png'
             cv.resize(square_img, down_points, cv.INTER_LINEAR)
         cv.imwrite(dst_dir_img, square_img)
 
