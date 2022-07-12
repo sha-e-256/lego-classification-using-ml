df9e8f7b0b66ea584d3b69bcf661616a23f55f2b
sha-e-256 <105937174+sha-e-256@users.noreply.github.com>
2022-07-12 12:48:54 -0400

    set background of test image to white by applying a mask

--- test.py
+++ test.py
@@ -31,6 +31,13 @@ def main():
     def create_JSON_annotation_file(img, dst_dir):
         max_border = 0
         contours_array = sc.get_contours(img)
+
+        img_g = cv.cvtColor(img, cv.COLOR_BGR2GRAY)  # Convert to greyscale
+        threshold = cv.threshold(img_g, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)[0]
+        mask = cv.inRange(img_g, threshold, 255)  # Create a mask of all pixels where the
+                                                  # pixel value is from threshold to 255 (white)
+        img_copy = img.copy()
+        img_copy[mask > threshold] = (255, 255, 255)
         data = {}  # Create a dictionary
         for i in range(len(contours_array)):
             contour = contours_array[i]
@@ -38,7 +45,7 @@ def main():
             # If angle changes, center coordinates and width/height do not change
             b_rect = cv.minAreaRect(contour)  # The center coordinates, width, height, and angle of rotation of b box
             rotation_matrix = cv.getRotationMatrix2D(center=b_rect[0], angle=b_rect[2], scale=1)
-            rotated_img = cv.warpAffine(src=img, M=rotation_matrix, dsize=(1280, 720))
+            rotated_img = cv.warpAffine(src=img_copy, M=rotation_matrix, dsize=(1280, 720))
 
             b_box = np.int0(cv.boxPoints(b_rect))  # Un-rotated corner coordinates
             min_x = b_box[0, 0] - 2
@@ -70,12 +77,16 @@ def main():
             max_border = sc.find_max_border(max_border, min_x, min_y, max_x, max_y, c_x, c_y)
             cropped_img = rotated_img[rotated_min_y:rotated_max_y, rotated_min_x:rotated_max_x]
             #cv.drawContours(rotated_img, [b_box], 0, (0, 0, 255), 1)
-            cv.imshow("Image w/ Bounding Box", cropped_img)
-            cv.waitKey()
-            cv.destroyAllWindows()
+            # cv.imshow("img with no bg", cropped_img)
+            # cv.waitKey()
+            # cv.destroyAllWindows()
             # draw_bounding_box(img, min_x, min_y, max_x, max_y)
+
+        # cv.imshow("img with no bg", img_copy)
+        # cv.waitKey()
+        # cv.destroyAllWindows()
         write_to_JSON_file(data)
-        sc.smart_crop(img, max_border, dst_dir)
+        sc.smart_crop(img_copy, max_border, dst_dir)
 
     dst_dir = r'E:\lego-classification-using-ml\segmented-testing-images'
     # On laptop E drive; on PC D drive
