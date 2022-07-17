8affa4cc3ff37fc828fd602da309293545db8764
sha-e-256 <105937174+sha-e-256@users.noreply.github.com>
2022-07-17 14:36:41 -0400

    change destination directory to not include file extension

--- dataset.py
+++ dataset.py
@@ -75,7 +75,7 @@ def main():
                     except OSError as error:
                         pass    # Do not generate a subdirectory if it already
                         # exists
-                    img_dst_dir = rf'{dst_dir}\{class_name}\{img_name}'
+                    img_dst_dir = rf'{dst_dir}\{class_name}\{img_name.split(".")[0]}'
                     sc.rotate_and_square_crop(img, img_dst_dir)
 
     process_images()
