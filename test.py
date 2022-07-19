1c991a6e17a854d08ecc12632a7be9275ad0d721
sha-e-256 <105937174+sha-e-256@users.noreply.github.com>
2022-07-19 00:47:19 -0400

    refactor code for demo so that input image is segmented

--- test.py
+++ test.py
@@ -1,105 +1,77 @@
-import cv2 as cv
-import smart_crop as sc
-import json
-import numpy as np
 import os
 
-__author__ = "Shahed E."
+import cv2 as cv
+import smart_crop as sc
+import numpy as np
+import random
+
 
 # The objective of this program is to take an image of a pile of scattered pieces
 # and segment it into several images
 # The coordinates of the bounding box and the centroid of each Lego piece
 # will be saved in a JSON file
 
+
+
 def main():
 
-    def draw_bounding_box(img, min_x, min_y, max_x, max_y):
-        cv.rectangle(img, (min_x, min_y), (max_x, max_y ), (0, 255, 0), 1)
-        cv.imshow("Image w/ Bounding Box", img)
+    # Create a dictionary that contains information on the bounding box coordinates
+    # and the model prediction & probabilities of each Lego piece so that this information
+    # can be drawn on the unsegmented image taken by the camera
+    def run_test(src_dir, dst_dir):
+        results = {}  # A dict containing the prediction, probability of prediction, coordinates
+        # of the four corners of the bounding box of each piece,
+        # and the center coordinates of each piece
+        unsegmented_img_src_dir = rf'{src_dir}\0.png'
+        unsegmented_img = cv.imread(unsegmented_img_src_dir)  # The image of the scattered Lego pieces
+        # taken by the camera
+        unsegmented_img_copy = unsegmented_img.copy()
+        sc.rotate_and_square_crop(unsegmented_img, dst_dir)  # Generate segmented images
+        true_contours = sc.get_true_contours()
+        # and return a list of the contours enclosing a piece (the 'true contours') in the unsegmented_img
+        segmented_img_names = os.listdir(dst_dir)
+        segmented_img_names_enum = enumerate(segmented_img_names)
+        for i, segmented_img_name in segmented_img_names_enum:
+            segmented_img_src_dir = rf'{dst_dir}\{segmented_img_name}'  # Path of the segmented image
+            segmented_img = cv.imread(segmented_img_src_dir)  # NumPy array of the segmented image
+
+            # ADD CODE HERE:
+            # segmented_img: input to the model
+            prediction = 'Plate 4 x 4 Corner'  # prediction: output from model
+            probability = 0.900  # 3 decimals  # probability: output from model
+            # Once prediction has been determined, update the dictionary
+            segmented_img_rect = cv.minAreaRect(true_contours[i])
+            (segmented_img_b_box_c_x, segmented_img_b_box_c_y) = sc.get_bounding_box_info(segmented_img_rect)[1]
+            segmented_img_b_box = np.int0(cv.boxPoints(segmented_img_rect))  # Coordinates of the four corners
+
+            results[i] = {}
+            results[i]['label'] = prediction
+            results[i]['probability'] = probability
+            results[i]['bounding_box'] = segmented_img_b_box
+            results[i]['c_x'] = segmented_img_b_box_c_x
+            results[i]['c_y'] = segmented_img_b_box_c_y
+
+
+        for j in range(len(results)):
+            segmented_img_b_box = results[j]['bounding_box']
+            colour = (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256))
+
+            cv.drawContours(unsegmented_img_copy, [segmented_img_b_box], 0, colour, 10)
+            img_height = int(unsegmented_img_copy.shape[0])
+            img_width = int(unsegmented_img_copy.shape[1])
+            down_points = (img_width//4, img_height//4)
+            unsegmented_img_downsized = cv.resize(src=unsegmented_img_copy,
+                                      dsize=down_points,
+                                      interpolation=cv.INTER_LINEAR)
+        cv.imshow("unsegmented img with bounding boxes", unsegmented_img_downsized)
         cv.waitKey()
         cv.destroyAllWindows()
 
-    def get_upright_bounding_box():
-        pass
+    dst_dir = rf'D:\lego-classification-using-ml\segmented-testing-images'
+    src_dir = rf'D:\lego-classification-using-ml\testing-images'
 
-    def write_to_JSON_file(data):
+    run_test(src_dir, dst_dir)
 
-        with open('segmented-images.json', 'w') as file:
-            json.dump(data, file, indent=4)
-        file.close()
-
-    def create_JSON_annotation_file(img, dst_dir):
-        max_border = 0
-        contours_array = sc.get_contours(img)
-
-        # If the contour is very small, then just disregard it; its probably not enclosing an object then
-        img_g = cv.cvtColor(img, cv.COLOR_BGR2GRAY)  # Convert to greyscale
-        #img_g_blur = cv.GaussianBlur(img_g, (5, 5), 0)
-        threshold = cv.threshold(img_g, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)[0]
-        mask = cv.inRange(img_g, threshold, 255)  # Create a mask of all pixels where the
-                                                  # pixel value is from threshold to 255 (white)
-        img_copy = img.copy()
-        img_copy[mask > threshold] = (255, 255, 255)
-        data = {}  # Create a dictionary
-        for i in range(len(contours_array)):
-            contour = contours_array[i]
-            # If angle changes, center coordinates and width/height do not change
-            b_rect = cv.minAreaRect(contour)  # The center coordinates, width, height, and angle of rotation of b box
-            rotation_matrix = cv.getRotationMatrix2D(center=b_rect[0], angle=b_rect[2], scale=1)
-            rotated_img = cv.warpAffine(src=img_copy, M=rotation_matrix, dsize=(800, 400))
-
-            b_box = np.int0(cv.boxPoints(b_rect))  # Un-rotated corner coordinates
-            min_x = b_box[0, 0]
-            max_x = b_box[2, 0]
-            min_y = b_box[1, 1]
-            max_y = b_box[3, 1]
-
-            # Tuples are immutable
-            (c_x, c_y) = b_rect[0] # Used for rotated and un-rotated images
-
-            (width, height) = b_rect[1]
-
-            if(width > 20 and height > 20):  # Ignore small bounding boxes; its not enclosing a shape
-                rotated_b_rect = (b_rect[0], b_rect[1], 0)  # Bounding box of piece after rotating image
-                rotated_b_box = np.int0(cv.boxPoints(rotated_b_rect))  # The coordinates of the four corners of the bounding box
-                # print(b_box)
-                rotated_min_x = rotated_b_box[0, 0] - 2
-                rotated_max_x = rotated_b_box[2, 0] + 4
-                rotated_min_y = rotated_b_box[1, 1] - 2
-                rotated_max_y = rotated_b_box[3, 1] + 4
-
-                data[i] = {}
-                data[i]['label'] = 'TBA'
-                data[i]['min_x'] = int(min_x)
-                data[i]['min_y'] = int(min_y)
-                data[i]['max_x'] = int(max_x)
-                data[i]['max_y'] = int(max_y)
-                data[i]['c_x'] = int(c_x)
-                data[i]['c_y'] = int(c_y)
-
-                max_border = sc.find_max_border(max_border, min_x, min_y, max_x, max_y, c_x, c_y)
-                #cropped_img = rotated_img[rotated_min_y:rotated_max_y, rotated_min_x:rotated_max_x]
-                cv.drawContours(img_copy, [b_box], 0, (0, 0, 0), 1)
-                # cv.imshow("img with no bg", img_copy)
-                # cv.waitKey()
-                # cv.destroyAllWindows()
-                # draw_bounding_box(img, min_x, min_y, max_x, max_y)
-
-        # cv.imshow("img with no bg", img_copy)
-        # cv.waitKey()
-        # cv.destroyAllWindows()
-        write_to_JSON_file(data)
-        white = [255, 255, 255]
-        img_copy = cv.copyMakeBorder(img_copy, 100, 100, 100, 100, cv.BORDER_CONSTANT, None,
-                                white)  # Add a white border
-        sc.smart_crop(img_copy, max_border, dst_dir)
-
-    dst_dir = r'E:\lego-classification-using-ml\segmented-testing-images'
-    # On laptop E drive; on PC D drive
-    src_dir = r'E:\lego-classification-using-ml\testing-images\04.png'
-    img = cv.imread(src_dir)
-
-    create_JSON_annotation_file(img, dst_dir)
 
 if __name__ == '__main__':
     main()
